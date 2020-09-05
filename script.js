31c6a46d400776c6f16ef4c5a21f20582513ffd5
Felipe Layos <jfelipelayos@gmail.com>
2020-09-05 14:36:39 -0500

    New version

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 function goTop(){
 	window.addEventListener('scroll', () => {
 			var scroll = document.documentElement.scrollTop;
+			console.log(scroll)
 			var goTopBtn = document.getElementById('gotop__btn');
-
 			if (scroll > 450) {
-				goTopBtn.style.right = 35 + 'px';
+				goTopBtn.style.right = 30 + 'px';
 			}
 			else{
 				goTopBtn.style.right = -100 + 'px';
@@ -14,7 +14,18 @@ function goTop(){
 			goTopBtn.addEventListener('click', () => {
 				document.documentElement.scrollTop = 0;
 			});
+
+			
+	});
+}
+
+function seeProjects(){
+	var seeProjectsBtn = document.getElementById('seeProjects__btn');
+	seeProjectsBtn.addEventListener('click', () => {
+		console.log("CLIQUEADO")
+		document.documentElement.scrollTop = 1900;
 	});
 }
 
 goTop();
+seeProjects();
